bf5f4cd12496d59a35734fa1764e806e7b8f54ae
Philbert Heng <iforte.philbert_heng@mercedes-benz.com>
2023-08-29 14:24:52 +0800

    added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 
+// Importing icons
+
+// End of importing icons
+
+const icons = {
+
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxBootstrapIconsModule.pick(icons)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
