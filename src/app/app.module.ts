f554e41e6ed71da663af7aded7b27697ab08c501
Philbert Heng <iforte.philbert_heng@mercedes-benz.com>
2023-08-30 17:19:30 +0800

    completed calculator page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { NgToastModule } from 'ng-angular-popup'
+
 import { CalculatorComponent } from './calculator/calculator.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { FaqComponent } from './faq/faq.component';
@@ -25,7 +29,10 @@ const icons = { }
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgxBootstrapIconsModule.pick(icons)
+    NgxBootstrapIconsModule.pick(icons),
+    FormsModule,
+    ReactiveFormsModule,
+    NgToastModule
   ],
   providers: [],
   bootstrap: [AppComponent],
